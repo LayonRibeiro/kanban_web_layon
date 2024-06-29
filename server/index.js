90327cd00b71e9ad4e8e11fd683e01858b3f74c4
Layon <layonjroliveira@gmail.com>
2024-06-29 18:32:38 -0300

    Add gitigone in client

--- server/index.js
+++ server/index.js
@@ -1,22 +1,25 @@
-import express from 'express';
-import connectDataBase from './src/database/db.js';
-import dotenv from 'dotenv';
+import express from "express";
+import connectDataBase from "./src/database/db.js";
+import dotenv from "dotenv";
+import cors from "cors";
 
-import userRoute from './src/routes/user.route.js';
-import authRoute from './src/routes/auth.route.js';
-import kanbanRoute from './src/routes/kanban.route.js';
-
+import userRoute from "./src/routes/user.route.js";
+import authRoute from "./src/routes/auth.route.js";
+import boardRoute from "./src/routes/board.route.js";
+import swaggerRoute from "./src/routes/swagger.route.js";
 
 dotenv.config();
 
-const app = express()
-const port = process.env.PORT || 3000 //Porta que o servidor disponibiliza OU a porta 3000
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 connectDataBase();
-app.use(express.json()); // mandar dados json para nossa api
+app.use(express.json());
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
-app.use("/kanban", kanbanRoute);
+app.use("/board", boardRoute);
+app.use("/docs", swaggerRoute);
 
-
-app.listen(port, () => console.log(`Servidor Rodando na porta ${port}`))
+app.listen(port, () => console.log(`Server running on port ${port}`));
